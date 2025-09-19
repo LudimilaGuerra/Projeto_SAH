55b09f6ab8b215957dcba8831293bbf9cb35a027
luizt <luizt@192.168.1.6>
2025-09-19 00:30:20 -0300

    Criação da classe User com seus atributos e seus getters/setters

---
+++ src/main/java/ucb/sah/model/User.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucb.sah.model;
+
+/**
+ *
+ * @author luizt
+ */
+public class User {
+    private String cpf;
+    private String nome;
+    private int idade;
+    
+    public User(String cpf, String nome, int idade){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade  = idade;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+}
