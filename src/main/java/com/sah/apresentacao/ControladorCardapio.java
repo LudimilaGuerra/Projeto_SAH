9eb41c6bce4d2c66ae8ebd15035881da675a8b94
Kaio <augustokaio02@gmail.com>
2025-09-19 13:52:37 -0300

    Estrutura MVC inicial

---
+++ src/main/java/com/sah/apresentacao/ControladorCardapio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sah.apresentacao;
+
+/**
+ *
+ * @author porti
+ */
+public class ControladorCardapio {
+    // Aqui futuramente vai o código que liga a visão com o domínio
+}
